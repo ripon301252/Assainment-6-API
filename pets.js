9da52192f20403da1375962104b7bfc2ae5482c9
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2024-12-22 05:59:59 +0600

    Update pets.js

--- pets.js
+++ pets.js
@@ -1,4 +1,32 @@
-fetch('https://openapi.programming-hero.com/api/peddy/pets')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err)) 
+// fetch, load and show categories on html
+
+    // create loadCategories
+    const loadCategories = () =>{
+        
+        // fetch the data
+        fetch('https://openapi.programming-hero.com/api/peddy/categories')
+            .then((res) => res.json())
+            .then((data) => displayCategories(data.categories))
+            .catch((err) => console.log(err))
+
+    }
+
+    // create displayCategories
+    const displayCategories = (category) =>{
+        const categoryContainer = document.getElementById('categories');
+
+        category.forEach((item) => {
+            
+            // create buttons
+            const button = document.createElement('button');
+            button.classList = 'btn';
+            button.innerText = item.category;
+
+            // add button to categoryContainer
+            categoryContainer.append(button)
+
+        });
+    }
+
+    loadCategories()
+    
